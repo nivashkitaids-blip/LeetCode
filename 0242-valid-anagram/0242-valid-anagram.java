1c1028030e73afdf63bedab31098af036d14d2af
Nivash <nivashkitaids@gmail.com>
2026-07-24 11:08:45 +0530

    Time: 6 ms (43.23%), Space: 44.5 MB (60.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,13 +6,13 @@ class Solution {
 
         int[] count = new int[26];
 
-        // Count frequency of characters in s1 and decrement for s2
+        
         for (int i = 0; i < s1.length(); i++) {
             count[s1.charAt(i) - 'a']++;
             count[s2.charAt(i) - 'a']--;
         }
 
-        // Check if all counts are zero
+        
         for (int c : count) {
             if (c != 0) {
                 return false;
