b78b0a236c4e52c4caace0fa2df0e6b22eec0e3c
Nivash <nivashkitaids@gmail.com>
2026-07-24 11:59:10 +0530

    Time: 6 ms (8.97%), Space: 48 MB (11.82%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean Palindrome(String s) {
+        int i =0,j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+
+    public boolean validPalindrome(String s) {
+        int i =0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=(s.charAt(j))){
+                String temp = s.substring(0,i) + s.substring(i+1,s.length());
+                String temp2= s.substring(0,j)+ s.substring(j+1,s.length());
+                if(Palindrome(temp) || Palindrome(temp2)){
+                    return true;
+                }
+                else return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+        
+
+
+    }
+}
