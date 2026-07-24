28d5eea9a539a8b8ad43cecdec054cb857ecfcdc
Nivash <nivashkitaids@gmail.com>
2026-07-24 14:53:44 +0530

    Time: 16 ms (51.4%), Space: 45.4 MB (42.7%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,50 +1,39 @@
 class Solution {
-
-    public int validpalin(int i, int j, String s) {
-
-        int count = 0;
-
-        while (i >= 0 && j < s.length()) {
-
-            if (s.charAt(i) != s.charAt(j)) {
+    public int validpalin(int i, int j, String s){
+        int count =0;
+        while(i>=0 && j<s.length()){
+            if(s.charAt(i) != s.charAt(j)){
                 return count;
             }
-
             count++;
             i--;
             j++;
         }
-
         return count;
     }
-
     public String longestPalindrome(String s) {
-
-        String res = "";
-        int resLength = 0;
-
-        for (int k = 0; k < s.length(); k++) {
-
-            // Odd length palindrome
-            int len = validpalin(k, k, s);
-
-            if (2 * len - 1 > resLength) {
-                resLength = 2 * len - 1;
-                res = s.substring(k - len + 1, k + len);
-            }
-
-            // Even length palindrome
-            if (k < s.length() - 1 && s.charAt(k) == s.charAt(k + 1)) {
-
-                len = validpalin(k, k + 1, s);
-
-                if (2 * len > resLength) {
-                    resLength = 2 * len;
-                    res = s.substring(k - len + 1, k + len + 1);
+        String res ="";
+        int  reslength = 0;
+        for(int k =0; k<s.length();k++){
+            if(k<s.length()-1 && s.charAt(k) == s.charAt(k+1)){
+                int len = validpalin(k,k+1,s);
+                if(2*len+2>reslength){
+                    reslength = 2*len+2;
+                    res=s.substring(k-len+1,k+len+1);
                 }
             }
-        }
+            
+            int len = validpalin(k,k,s);
+            if(2*len +1 >reslength){
+                reslength = 2*len+1;
+                res = s.substring(k-len+1,k+len);
+                        
+                    }
 
-        return res;
+                
+                
+            }
+            return res;
+        }
+        
     }
-}
