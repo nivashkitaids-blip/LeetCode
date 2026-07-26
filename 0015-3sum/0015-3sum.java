ec2b65d7354708e78bdd5417e6b2a73f00cd4089
Nivash <nivashkitaids@gmail.com>
2026-07-26 20:38:20 +0530

    Time: 34 ms (61.18%), Space: 59.1 MB (60.02%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,43 +1,39 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        
         Arrays.sort(nums);
         
-        int n = nums.length;
-        
-        for (int i = 0; i < n - 2; i++) {
-            
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        List<List<Integer>> out = new ArrayList<>();
+        for(int i =0; i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                
                 continue;
             }
-            
-            int j = i + 1;
-            int k = n - 1;
-            
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
                 
-                if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum == 0){
+                    out.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    
                     j++;
                     k--;
-                    
-                    
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 
-                     1]) k--;
-                    
-                    
-                } else if (sum < 0) {
-                    j++; 
-                } else {
-                    k--; 
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k] == nums[k+1])k--;
+                                      
                 }
+              
+                
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    k--;
+                }    
             }
         }
+        return out;
         
-        return res;
     }
 }
