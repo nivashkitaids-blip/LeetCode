109466b3a33625166d1348391c6698083602a900
Nivash <nivashkitaids@gmail.com>
2026-08-27 12:25:52 +0530

    Time: 0 ms (100%), Space: 46.7 MB (69.02%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,23 +14,24 @@
  * }
  */
 class Solution {
-    public void traverse(TreeNode node, List<Integer> list) {
-        if (node == null) return;
-        list.add(node.val);
-        traverse(node.left, list);
-        traverse(node.right, list);
+    public static int count;
+    public static int answer;
+    public void inOrder(TreeNode root,int k){
+        if(root == null) return;
+        inOrder(root.left,k);
+        count++;
+        if(k==count){
+            answer = root.val;
+            return;
+        }
+        inOrder(root.right,k);
     }
-
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> list = new ArrayList<>();
-        // Step 1: Collect all elements
-        traverse(root, list);
-        
-        // Step 2: Sort the elements
-        Collections.sort(list);
-        
-        // Step 3: Return the (k-1)-th element
-        return list.get(k - 1);
+        count = 0;
+        answer = 0;
+        inOrder(root,k);
+        return answer;
+
         
     }
 }
