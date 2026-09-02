7a2878d771af4f37f0c48843cf9c701a436af6f8
Nivash <nivashkitaids@gmail.com>
2026-09-02 10:21:16 +0530

    Time: 8 ms (60.28%), Space: 47 MB (18.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         return calculateWays(nums, 0, 0, target);
     }
@@ -13,5 +13,42 @@ class Solution {
         int addWays = calculateWays(nums, index + 1, currentSum + nums[index], target);
         int subtractWays = calculateWays(nums, index + 1, currentSum - nums[index], target);
         return addWays + subtractWays;
+    }
+} */
+import java.util.Arrays;
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
+
+        // If target is out of achievable range
+        if (Math.abs(target) > totalSum) return 0;
+
+        // DP table initialized with -1
+        // Size of sum range is [-totalSum, totalSum], offset by totalSum
+        int[][] memo = new int[nums.length][2 * totalSum + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+
+        return calculateWays(nums, 0, 0, target, totalSum, memo);
+    }
+
+    private int calculateWays(int[] nums, int index, int currentSum, int target, int totalSum, int[][] memo) {
+        if (index == nums.length) {
+            return currentSum == target ? 1 : 0;
+        }
+
+        // Offset index for negative sums
+        int memoSumIndex = currentSum + totalSum;
+        if (memo[index][memoSumIndex] != -1) {
+            return memo[index][memoSumIndex];
+        }
+
+        int addWays = calculateWays(nums, index + 1, currentSum + nums[index], target, totalSum, memo);
+        int subtractWays = calculateWays(nums, index + 1, currentSum - nums[index], target, totalSum, memo);
+
+        return memo[index][memoSumIndex] = addWays + subtractWays;
     }
 }
